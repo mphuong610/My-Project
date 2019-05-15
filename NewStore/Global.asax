72a99ce7f1cba850ae2d58d7a37ce919bdcf38fb
Untitled <mphuong61097@gmail.com>
2019-05-16 00:02:11 +0700

    Add project files.

---
+++ NewStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewStore.MvcApplication" Language="C#" %>
---
+++ NewStore/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace NewStore.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NewStore.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(NewStore.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
